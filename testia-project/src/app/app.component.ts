ca18239eec9169a46ec9acc4b2fb1e5be5985d22
Zenryou <cedric.rano@hotmail.fr>
2021-10-19 17:28:56 +0200

    Import img in canvas without resize

--- testia-project/src/app/app.component.ts
+++ testia-project/src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, ngAfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements ngAfterViewInit {
 
-  @ViewChild('canvasId', {static: false}) myCanvas: ElementRef;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
 
+  private canvasNativeElement: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+
+  file: File | null = null;
+  imgLoad = false;
   isZoom = false;
 
+  ngAfterViewInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.canvasNativeElement = this.canvas.nativeElement;
+    this.canvasNativeElement.width = 900;
+    this.canvasNativeElement.height = 600;
+  }
 
-  importImg() {
-
+  importImg(files: FileList) {
+    this.file = files.item(0);
+    this.cleanCanvas();
+    this.drawImage();
+    this.imgLoad = true;
   }
 
   draw() {
@@ -22,5 +36,21 @@ export class AppComponent {
 
   toggleZoom() {
     this.isZoom = !this.isZoom;
+  }
+
+  private drawImage(): void {
+    if (this.file) {
+      const img = new Image;
+      img.onload = () => {
+          this.canvasNativeElement.width = img.width;
+          this.canvasNativeElement.height = img.height;
+          this.ctx.drawImage(img, 0, 0, img.width, img.height);
+      }
+      img.src = URL.createObjectURL(this.file);
+    }
+  }
+
+  private cleanCanvas(): void {
+    this.ctx.clearRect(0, 0, this.canvasNativeElement.width, this.canvasNativeElement.height);
   }
 }
